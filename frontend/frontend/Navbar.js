16da09678bfe8b1ec85ea98500bef6fc8e228307
Aqsa Amin <aqsa@example.com>
2025-09-24 21:25:01 +0500

    Changed navbar color to light peach

--- frontend/frontend/Navbar.js
+++ frontend/frontend/Navbar.js
@@ -6,14 +6,22 @@ function Navbar() {
   const { cart } = useContext(CartContext);
 
   return (
-    <nav style={{ padding: "1rem", background: "#000", color: "#fff" }}>
-      <Link to="/" style={{ color: "#800000", fontWeight: "bold", fontSize: "1.5rem" ,fontFamily:"fantasy"}}>
+    <nav style={{ padding: "1rem", background: "#FFDAB9", color: "#000" }}>
+      <Link 
+        to="/" 
+        style={{ 
+          color: "#000", 
+          fontWeight: "bold", 
+          fontSize: "1.5rem", 
+          fontFamily: "fantasy" 
+        }}
+      >
         Mahnoor Collections
       </Link>
       <span style={{ marginLeft: "2rem" }}>
-        <Link to="/" style={{ color: "#fff", marginRight: "1rem" }}>Home</Link>
-        <Link to="/shop" style={{ color: "#fff", marginRight: "1rem" }}>Shop</Link>
-        <Link to="/cart" style={{ color: "#fff" }}>
+        <Link to="/" style={{ color: "#000", marginRight: "1rem" }}>Home</Link>
+        <Link to="/shop" style={{ color: "#000", marginRight: "1rem" }}>Shop</Link>
+        <Link to="/cart" style={{ color: "#000" }}>
           Cart ({cart.reduce((acc, p) => acc + p.quantity, 0)})
         </Link>
       </span>
